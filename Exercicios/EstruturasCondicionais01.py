ce19d5507c4ea9a9cd4eeb92ec767fcaaba6199e
Neissandy <neisandy.silva@gmail.com>
2023-09-02 14:35:57 -0300

    commit 6

--- Exercicios/EstruturasCondicionais01.py
+++
@@ -1,7 +0,0 @@
-login = input("Digite o seu login:")
-senha = input("Digite sua senha:")
-
-if login == "userpy" and senha == "teste123":
-    print("Bem-vindo userpy")
-else:
-    print("Login falhou, verifique sua senha e tente novamente")
